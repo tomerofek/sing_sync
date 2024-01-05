5a2b838f84bc8d7646bde2fbddd1d0f7caff4d97
Tomer Ofek <tomerofe@post.bgu.ac.il>
2024-01-05 20:17:38 +0200

    11.1

--- backend/src/data.ts
+++ backend/src/data.ts
@@ -1,7 +1,6 @@
-import {Food} from './app/shared/models/Food';
-import { Tag } from './app/shared/models/Tag';
 
-export const sample_foods: Food[] = [
+
+export const sample_foods: any[] = [
   {
     id:'1',
     name: 'Pizza Pepperoni',
@@ -70,7 +69,7 @@ export const sample_foods: Food[] = [
   },
 ]
 
-export const sample_tags:Tag[] = [
+export const sample_tags:any[] = [
     { name: 'All', count: 6 },
     { name: 'FastFood', count: 4 },
     { name: 'Pizza', count: 2 },
