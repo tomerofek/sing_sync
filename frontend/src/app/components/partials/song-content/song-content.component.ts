c795c85b8c250fe9567b0c9f2a77c84f9faab886
Daniel-Pikovsky <dan13161316@gmail.com>
2024-02-06 18:25:12 +0200

    added song and songLine frontend models, added frontend song service, and added partial implementation of song-content component

--- frontend/src/app/components/partials/song-content/song-content.component.ts
+++ frontend/src/app/components/partials/song-content/song-content.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SongService } from 'src/app/services/song.service';
+import { Song } from 'src/app/shared/models/Song';
 
 @Component({
   selector: 'song-content',
@@ -6,10 +9,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./song-content.component.css']
 })
 export class SongContentComponent implements OnInit {
+  song:Song;
+  show_chords:boolean = false;
 
-  constructor() { }
+  constructor(activatedRoute:ActivatedRoute, songService:SongService,
+          private router: Router) {
+    /*activatedRoute.params.subscribe((params) => {
+      if(params.song) this.song = params.song;
+    });*/
+    this.song = songService.generate_test_Song();
+  }
 
   ngOnInit(): void {
+  }
+
+  switchChords():void{
+    this.show_chords = !this.show_chords;
+    //console.log(this.show_chords);
   }
 
 }
---
+++ frontend/src/app/shared/models/Song.ts
@@ -0,0 +1,13 @@
+import { SongLine } from "./SongLine";
+
+export class Song{
+    song_name!: string;
+    song_author!: string;
+    song_body!: SongLine[];
+
+    constructor(n:string, a:string, b: SongLine[]){
+        this.song_name=n;
+        this.song_author=a;
+        this.song_body=b;
+    }
+}
