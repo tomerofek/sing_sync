1f474be1b1a40c0a76f2c7d6106de8705105a627
lior-yishay <lioryi@post.bgu.ac.il>
2024-02-07 14:13:01 +0200

    adding template for the queue popup

--- frontend/src/app/components/pages/queue-view/queue-view.component.ts
+++ frontend/src/app/components/pages/queue-view/queue-view.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QueueViewComponent implements OnInit {
 
-  constructor() { }
+  currentSong;
+  queue : songDemo[] = [];
+
+  constructor() {
+    this.queue = [new songDemo("תשאירי לי מקום לחבק אותך", "אבטיפוס"),
+                  new songDemo("יסמין", "הפיל הכחול"),
+                  new songDemo("ערק", "פאר טסי"),
+                  new songDemo("איש של לילה", "רוני דלומי")];
+    this.currentSong = this.queue.shift();
+   }
 
   ngOnInit(): void {
+  }
+
+}
+
+class songDemo {
+  name: string = '';
+  author: string = '';
+
+  constructor(name : string, author : string){
+    this.name = name;
+    this.author = author;
   }
 
 }
