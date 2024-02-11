41e17b377d079a58ea149818fd545586eead9b2e
lior-yishay <lioryi@post.bgu.ac.il>
2024-02-11 17:00:30 +0200

    changing queue component to work with song class

--- frontend/src/app/components/partials/queue/queue.component.ts
+++ frontend/src/app/components/partials/queue/queue.component.ts
@@ -10,10 +10,18 @@ import { QueueViewComponent } from '../../pages/queue-view/queue-view.component'
 })
 export class QueueComponent implements OnInit {
 
-  firstInQ = 'יסמין - הפיל הכחול';
-  secondInQ = 'ערק - פאר טסי'
+  topOfQ : songDemo[] = [];
+  firstInQ;
+  secondInQ;
 
-  constructor(private dialog: MatDialog) { }
+  constructor(private dialog: MatDialog) {
+    this.topOfQ = [ new songDemo('יסמין', 'הפיל הכחול'),
+                    new songDemo('ערק', 'פאר טסי')];
+
+    this.firstInQ = this.topOfQ.shift();
+    this.secondInQ = this.topOfQ.shift();
+    
+  }
 
   ngOnInit(): void {
   }
@@ -25,4 +33,14 @@ export class QueueComponent implements OnInit {
   showAddSongDialog(): void {
   }
 
+}
+
+class songDemo {
+  name: string = '';
+  author: string = '';
+
+  constructor(name : string, author : string){
+    this.name = name;
+    this.author = author;
+  }
 }
