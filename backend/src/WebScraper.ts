289261012e059074dfdc68a82a10c47e8dcac791
Your Name <zloofe@post.bgu.ac.il>
2024-04-11 23:11:26 +0300

    improved name extraction

--- backend/src/WebScraper.ts
+++ backend/src/WebScraper.ts
@@ -13,7 +13,8 @@ export class WebScraper {
 
     async add_song_name($: cheerio.Root, song_dict: ScrapedSong): Promise<void> {
         const title = $('h1').first();
-        song_dict.song_name = title.children().first().text().trim();
+        let name =  $('[itemprop="name"]').eq(3).text();
+        song_dict.song_name = name;
     }
 
     async add_song_author($: cheerio.Root, song_dict: ScrapedSong): Promise<void> {
