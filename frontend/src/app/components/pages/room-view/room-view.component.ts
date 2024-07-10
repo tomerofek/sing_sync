5c83fff0a4af299f3304057b06edffe393e07c2f
lior-yishay <lioryi@post.bgu.ac.il>
2024-07-10 12:58:12 +0300

    now updating 'is_last_song_part' when advacing a song or going to the prev song

--- frontend/src/app/components/pages/room-view/room-view.component.ts
+++ frontend/src/app/components/pages/room-view/room-view.component.ts
@@ -155,7 +155,7 @@ export class RoomViewComponent implements OnInit, OnChanges {
       }
       else{
         //the song will be updated in the broadcast
-        const newSong: Song | undefined = this.responseService.getContent(songRes);
+        this.is_last_song_part = false;
         this.getCurrentSongPartIndex();
         this.getTopQ();
         this.update_has_next_song();
@@ -235,10 +235,8 @@ export class RoomViewComponent implements OnInit, OnChanges {
         this.notificationService.openSnackBarError(this.snackBar, songRes === null ? 'היתה בעיה בחזור לשיר הקודם' : this.responseService.getError(songRes))
       }
       else{
-        const newSong: Song | undefined = this.responseService.getContent(songRes);
-        if(newSong){
-          this.song = this.songService.separate_song_body(newSong);
-        }
+        //the song will be updated in the broadcast
+        this.is_last_song_part = false;
         this.getCurrentSongPartIndex();
         this.getTopQ();
         this.update_has_next_song();
